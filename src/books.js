57ade978097adb86ce26a3af534fd74994f6a06a
Malik001-G <adisamalikng0@gmail.com>
2024-02-26 14:56:56 +0100

    Amazon bestsellers card ready

--- src/books.js
+++ src/books.js
@@ -1,15 +1,24 @@
-export const books = [
-    {
-      author: "Kristin Hannah",
-      title: "The Women: A Novel",
-      img: "./images/book1.jpg",
-      id: 1,
-    },
-    {
-      author: " James Clear ",
-      title: "Atomic Habits",
-      img: "https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg",
-      id: 2,
-    },
-  ];
+import img1 from "./images/book1.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.jpg";
 
+export const books = [
+  {
+    author: "Kristin Hannah",
+    title: "The Women: A Novel",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: " James Clear ",
+    title: "Atomic Habits",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Peter Schweizer",
+    title: "Blood Money",
+    img: img3,
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,37 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import {books} from './books'
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
   return (
-    <section className="bookList">
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />;
-      })}
-     
-    </section>
-  );
-};
-
-// const Book = ({ img, title, author }) => {
-//   return <article className="book">
-//     <img src= {img} alt={title} />
-//     <h2>{title}</h2>
-//     <h4>{author}</h4>
-//   </article>
-// }
-
-//Another way to do the props destructuring
-const Book = (props) => {
-  const { img, title, author } = props;
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-
-    </article>
+    <>
+      <h1>Amazon best sellers</h1>
+      <section className="bookList">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
