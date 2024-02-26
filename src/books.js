2e08ee73fda0ce7e9688753f9bdfe150032e848e
Malik001-G <adisamalikng0@gmail.com>
2024-02-26 12:50:35 +0100

    Es6 modules- Using exports

---
+++ src/books.js
@@ -0,0 +1,15 @@
+export const books = [
+    {
+      author: "Kristin Hannah",
+      title: "The Women: A Novel",
+      img: "./images/book1.jpg",
+      id: 1,
+    },
+    {
+      author: " James Clear ",
+      title: "Atomic Habits",
+      img: "https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg",
+      id: 2,
+    },
+  ];
+
--- src/index.js
+++ src/index.js
@@ -2,31 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-
-const books = [
-  {
-    author: "Kristin Hannah",
-    title: "The Women: A Novel",
-    img: "./images/book1.jpg",
-    id: 1,
-  },
-  {
-    author: " James Clear ",
-    title: "Atomic Habits",
-    img: "https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg",
-    id: 2,
-  },
-];
+import {books} from './books'
 
 const BookList = () => {
-  const someValue = 'shakeAndBake'
-  const displayValue = () => {
-    console.log(someValue);
-  }
   return (
     <section className="bookList">
       {books.map((book) => {
-        return <Book {...book} key={book.id} displayValue = {displayValue} />;
+        return <Book {...book} key={book.id} />;
       })}
      
     </section>
@@ -43,13 +25,11 @@ const BookList = () => {
 
 //Another way to do the props destructuring
 const Book = (props) => {
-  const { img, title, author, displayValue } = props;
-
+  const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayValue}>Click me</button>
       <h4>{author}</h4>
 
     </article>
